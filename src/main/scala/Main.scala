9f93ebc62acb57fa80467230b16f377001333ade
eren <eozel44@gmail.com>
2023-06-11 01:59:39 +0300

    service

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,45 +8,14 @@ import java.time.temporal.ChronoField
 
 object Main extends ZIOAppDefault {
 
-import domain.Speech._
+import service._
 
   val url = "https://fid-recruiting.s3-eu-west-1.amazonaws.com/politics.csv"
 
 
-  def sum(stream: ZStream[Any, Throwable, Option[Long]]): ZIO[Any, Throwable, Option[Long]] =
-    stream.runFold(Option.empty[Long]) {
-      case (None, some)       => some
-      case (Some(x), Some(y)) => Some(x+y)
-      case (some, None)       => some
-    }
-
-  def calculate(
-                          stream: ZStream[Any, Throwable, Speech]
-                        ): ZStream[Any, Throwable, (String, Option[Long])]                                          =
-    stream.groupBy(in => ZIO.succeed(in.speaker, in.wordCount)) { case (speaker, stream) =>
-      ZStream.fromZIO(sum(stream)).map(r => (speaker, r))
-    }
-
-
     val program = for {
 
-      res  <- Client.request(url).mapError(k => HttpClientError(k.getMessage))
-      zstream = res.body.asStream
-        .via(ZPipeline.utf8Decode >>> ZPipeline.splitLines)
-        .via(ZPipeline.drop(1))
-        .via(ZPipeline.mapZIO(in => ZIO.fromEither(decode[Speech](convertToJson(in))).mapError(k => ParsingError(k.getMessage()))))
-
-        //mostSpeechs   <- calculate(zstream.filter(in=> in.dateOfSpeech.get(ChronoField.YEAR).equals(2012))).runCollect
-        //mostSpeechsSpeaker = mostSpeechs.sortWith(_._2.getOrElse(0L) > _._2.getOrElse(0L)).head._1
-        //_ <- Console.printLine(mostSpeechsSpeaker)
-
-        mostSpecurity <- calculate(zstream.filter(in=> in.topic.equals("Kohlesubventionen"))).runCollect
-        mostSecuritysSpeaker = mostSpecurity.sortWith(_._2.getOrElse(0L) > _._2.getOrElse(0L)).head._1
-        _ <- Console.printLine(mostSecuritysSpeaker)
-
-//      leastWordy    <- calculate(zstream).runCollect
-//      leastWordySpeaker = leastWordy.sortWith(_._2.getOrElse(0L) < _._2.getOrElse(0L)).head._1
-//      _ <- Console.printLine(leastWordySpeaker)
+      res  <- SpeechService.calculateSpeechs(url)
 
     } yield res
 
---
+++ src/main/scala/service/SpeechService.scala
@@ -0,0 +1,51 @@
+package service
+
+import domain.{HttpClientError, ParsingError, Speech}
+import zio._
+import zio.http.Client
+import zio.stream._
+import io.circe.parser._
+
+object SpeechService {
+
+  import domain.Speech._
+
+  private def sum(stream: ZStream[Any, Throwable, Option[Long]]): ZIO[Any, Throwable, Option[Long]] =
+    stream.runFold(Option.empty[Long]) {
+      case (None, some)       => some
+      case (Some(x), Some(y)) => Some(x+y)
+      case (some, None)       => some
+    }
+
+  private def calculate(
+                 stream: ZStream[Any, Throwable, Speech]
+               ): ZStream[Any, Throwable, (String, Option[Long])]                                          =
+    stream.groupBy(in => ZIO.succeed(in.speaker, in.wordCount)) { case (speaker, stream) =>
+      ZStream.fromZIO(sum(stream)).map(r => (speaker, r))
+    }
+
+
+  def calculateSpeechs(url: String) = for {
+
+    res  <- Client.request(url).mapError(k => HttpClientError(k.getMessage))
+    zstream = res.body.asStream
+      .via(ZPipeline.utf8Decode >>> ZPipeline.splitLines)
+      .via(ZPipeline.drop(1))
+      .via(ZPipeline.mapZIO(in => ZIO.fromEither(decode[Speech](convertToJson(in))).mapError(k => ParsingError(k.getMessage()))))
+
+    //mostSpeechs   <- calculate(zstream.filter(in=> in.dateOfSpeech.get(ChronoField.YEAR).equals(2012))).runCollect
+    //mostSpeechsSpeaker = mostSpeechs.sortWith(_._2.getOrElse(0L) > _._2.getOrElse(0L)).head._1
+    //_ <- Console.printLine(mostSpeechsSpeaker)
+
+    mostSpecurity <- calculate(zstream.filter(in=> in.topic.equals("Kohlesubventionen"))).runCollect
+    mostSecuritysSpeaker = mostSpecurity.sortWith(_._2.getOrElse(0L) > _._2.getOrElse(0L)).head._1
+    _ <- Console.printLine(mostSecuritysSpeaker)
+
+    //      leastWordy    <- calculate(zstream).runCollect
+    //      leastWordySpeaker = leastWordy.sortWith(_._2.getOrElse(0L) < _._2.getOrElse(0L)).head._1
+    //      _ <- Console.printLine(leastWordySpeaker)
+
+  } yield res
+
+
+}
